fff8102d9faaafe4e84fe7187912784e97df0f3f
Pingg723 <154111371+Pingg723@users.noreply.github.com>
2023-12-25 10:49:43 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
+let soundModelURL = 'https://pingg723.github.io/frog01218/model.json';
 
 
 function preload() {
